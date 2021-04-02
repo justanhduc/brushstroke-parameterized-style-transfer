a587a1a78e438c28cfdbbdf6a62898d0953af6ab
Duc <adnguyen@yonsei.ac.kr>
2021-04-02 22:03:56 +0900

    decreased style weight

--- nst.py
+++ nst.py
@@ -192,7 +192,7 @@ def get_input_optimizer():
 
 
 def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, style_img, num_steps=20000,
-                       style_weight=1000000, content_weight=1):
+                       style_weight=100000, content_weight=1):
     """Run the style transfer."""
     print('Building the style transfer model..')
     model, style_losses, content_losses = get_style_model_and_losses(cnn,
