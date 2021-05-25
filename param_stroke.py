5985028cff49a5eb942fd3984d8695c7c3a4d5d9
Duc <adnguyen@yonsei.ac.kr>
2021-05-25 16:27:54 +0900

    optimized codes

--- param_stroke.py
+++ param_stroke.py
@@ -2,6 +2,7 @@ import torch as T
 from torch.nn import functional as F
 from torch_cluster import knn
 import numpy as np
+from torchvision.transforms import functional as TF
 
 import utils
 
@@ -61,51 +62,34 @@ def stroke_renderer(curve_points: T.Tensor, locations: T.Tensor, colors: T.Tenso
     coord_x = T.clamp(coord_x, 0, W)
     coord_y = T.clamp(coord_y, 0, H)
     locations = T.cat((coord_x, coord_y), dim=1)
-    widths = T.relu(widths)
+    widths = T.exp(widths)
 
     device = curve_points.device
     N, S, _ = curve_points.shape
+
     # define coarse grid cell
     t_H = T.linspace(0., float(H), int(H // 5)).to(device)
     t_W = T.linspace(0., float(W), int(W // 5)).to(device)
     P_y, P_x = T.meshgrid(t_H, t_W)
     P = T.stack([P_x, P_y], dim=-1)  # [32, 32, 2]
 
-    # Compute now distances from every brushtroke center to every coarse grid cell
-    # D_to_all_B_centers = T.sum((P[..., None, :] - locations) ** 2, dim=-1)  # [H // C, W // C, N]
     # Find nearest brushstrokes' indices for every coarse grid cell
-    idcs = knn(locations, P.view(-1, 2), k=K)[1]
+    indices = knn(locations, P.view(-1, 2), k=K)[1]
 
-    # [H // 10, W // 10, K, S, 2]
-    canvas_with_nearest_Bs = curve_points[idcs.view(-1)].view(len(t_H), len(t_W), K, S, 2)
+    # Resize the KNN index tensor to full resolution
+    indices = indices.view(len(t_H), len(t_W), -1)
+    indices = indices.permute(2, 0, 1)
+    indices = TF.resize(indices, size=(H, W), interpolation=TF.InterpolationMode.NEAREST)
+    indices = indices.permute(1, 2, 0)
 
-    # [H // 10, W // 10, K, 3]
-    canvas_with_nearest_Bs_colors = colors[idcs.view(-1)].view(len(t_H), len(t_W), K, 3)
+    # locations of points sampled from curves
+    canvas_with_nearest_Bs = curve_points[indices.flatten()].view(H, W, K, S, 2)
 
-    # [H // 10, W // 10, K, 1]
-    canvas_with_nearest_Bs_bs = widths[idcs.view(-1)].view(len(t_H), len(t_W), K, 1)
+    # colors of curves
+    canvas_with_nearest_Bs_colors = colors[indices.flatten()].view(H, W, K, 3)
 
-    # Resize those tensors to the full canvas size (not coarse grid)
-    # First locations of points sampled from curves
-    H_, W_, r1, r2, r3 = canvas_with_nearest_Bs.shape
-    canvas_with_nearest_Bs = canvas_with_nearest_Bs.view(1, H_, W_, r1 * r2 * r3)  # [1, H // 10, W // 10, K * S * 2]
-    canvas_with_nearest_Bs = F.interpolate(canvas_with_nearest_Bs.permute(0, 3, 1, 2), size=[H, W], mode='nearest')
-    canvas_with_nearest_Bs = canvas_with_nearest_Bs.permute(0, 2, 3, 1).view(H, W, r1, r2, r3)  # [H, W, N, S, 2]
-
-    # Now colors of curves
-    H_, W_, r1, r2 = canvas_with_nearest_Bs_colors.shape
-    canvas_with_nearest_Bs_colors = canvas_with_nearest_Bs_colors.view(1, H_, W_,
-                                                                       r1 * r2)  # [1, H // 10, W // 10, K * 3]
-    canvas_with_nearest_Bs_colors = F.interpolate(canvas_with_nearest_Bs_colors.permute(0, 3, 1, 2), size=(H, W),
-                                                  mode='nearest')
-    canvas_with_nearest_Bs_colors = canvas_with_nearest_Bs_colors.permute(0, 2, 3, 1).view(H, W, r1, r2)  # [H, W, K, 3]
-
-    # And with the brush size
-    H_, W_, r1, r2 = canvas_with_nearest_Bs_bs.shape
-    canvas_with_nearest_Bs_bs = canvas_with_nearest_Bs_bs.view(1, H_, W_, r1 * r2)  # [1, H // 10, W // 10, K]
-    canvas_with_nearest_Bs_bs = F.interpolate(canvas_with_nearest_Bs_bs.permute(0, 3, 1, 2), size=(H, W),
-                                              mode='nearest')
-    canvas_with_nearest_Bs_bs = canvas_with_nearest_Bs_bs.permute(0, 2, 3, 1).view(H, W, r1, r2)  # [H, W, K, 1]
+    # brush size
+    canvas_with_nearest_Bs_bs = widths[indices.flatten()].view(H, W, K, 1)
 
     # Now create full-size canvas
     t_H = T.linspace(0., float(H), H).to(device)
@@ -121,14 +105,17 @@ def stroke_renderer(curve_points: T.Tensor, locations: T.Tensor, colors: T.Tenso
     canvas_with_nearest_Bs_b_a = canvas_with_nearest_Bs_b - canvas_with_nearest_Bs_a  # [H, W, N, S - 1, 2]
     P_full_canvas_with_nearest_Bs_a = P_full[:, :, None, None, :] - canvas_with_nearest_Bs_a  # [H, W, K, S - 1, 2]
 
-    # compute t value for which each pixel is closest to each line that goes through each line segment (among nearest ones)
+    # find the projection of grid points on curves
+    # first find the projections of a grid point on each line segment of a curve
+    # numerator is the dot product between two vectors
+    # the first vector is the line segments. the second vector is the sample points -> grid
     t = T.sum(canvas_with_nearest_Bs_b_a * P_full_canvas_with_nearest_Bs_a, dim=-1) / (
             T.sum(canvas_with_nearest_Bs_b_a ** 2, dim=-1) + 1e-8)
 
     # if t value is outside [0, 1], then the nearest point on the line does not lie on the segment, so clip values of t
     t = T.clamp(t, 0., 1.)
 
-    # compute closest points on each line segment - [H, W, K, S - 1, 2]
+    # compute closest points on each line segment, which are the projections on each segment - [H, W, K, S - 1, 2]
     closest_points_on_each_line_segment = canvas_with_nearest_Bs_a + t[..., None] * canvas_with_nearest_Bs_b_a
 
     # compute the distance from every pixel to the closest point on each line segment - [H, W, K, S - 1]
@@ -136,14 +123,14 @@ def stroke_renderer(curve_points: T.Tensor, locations: T.Tensor, colors: T.Tenso
         (P_full[..., None, None, :] - closest_points_on_each_line_segment) ** 2, dim=-1)
 
     # and distance to the nearest bezier curve.
-    D = T.amin(dist_to_closest_point_on_line_segment, dim=(-1, -2))  # [H, W]
+    D_per_strokes = T.amin(dist_to_closest_point_on_line_segment, dim=-1)  # [H, W, K]
+    D = T.amin(D_per_strokes, dim=-1)  # [H, W]
 
     # Finally render curves on a canvas to obtain image.
-    I_NNs_B_ranking = F.softmax(100000. * (1.0 / (1e-8 + T.amin(dist_to_closest_point_on_line_segment, dim=-1))),
-                                dim=-1)  # [H, W, N]
+    I_NNs_B_ranking = F.softmax(100000. * (1.0 / (1e-8 + D_per_strokes)), dim=-1)  # [H, W, N]
     I_colors = T.einsum('hwnf,hwn->hwf', canvas_with_nearest_Bs_colors, I_NNs_B_ranking)  # [H, W, 3]
     bs = T.einsum('hwnf,hwn->hwf', canvas_with_nearest_Bs_bs, I_NNs_B_ranking)  # [H, W, 1]
-    bs_mask = T.sigmoid(bs - D[..., None])
+    bs_mask = T.sigmoid(bs - D[..., None])  # AOE of each brush stroke
     canvas = T.ones_like(I_colors) * canvas_color
     I = I_colors * bs_mask + (1 - bs_mask) * canvas
     return I  # HxWx3
@@ -188,7 +175,7 @@ class BrushStrokeRenderer(T.nn.Module):
         self.curve_c = T.nn.Parameter(T.from_numpy(np.array(c, 'float32')), requires_grad=True)
         self.color = T.nn.Parameter(T.from_numpy(color), requires_grad=True)
         self.location = T.nn.Parameter(T.from_numpy(np.array(location, 'float32')), requires_grad=True)
-        self.width = T.nn.Parameter(T.from_numpy(width), requires_grad=True)
+        self.width = T.nn.Parameter(T.from_numpy(np.log(np.minimum(width, 1e-3))), requires_grad=True)
 
     def forward(self):
         curve_points = sample_quadratic_bezier_curve(s=self.curve_s + self.location,
